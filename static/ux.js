7ca5cf7692e64b818914ce318f3c5372e70a6cd6
Will Scott <willscott@gmail.com>
2015-05-07 10:17:46 -0400

    button

--- static/ux.js
+++ static/ux.js
@@ -4,4 +4,15 @@ window.addEventListener('load', function () {
     loop: false,
     responsiveFallback: false
   });
+  setupDemo();
 }, true);
+
+function setupDemo() {
+  var ifere = document.getElementById('demo-button');
+  var rline = document.getElementsByClassName('right')[0];
+  var bcontent = document.getElementById('browserinter');
+  ifere.addEventListener('click', function () {
+    rline.style.display = 'none';
+    bcontent.style.display = 'block';
+  }, false);
+};
