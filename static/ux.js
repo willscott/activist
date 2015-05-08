3165022cc4a6bf7b1b3e0c6cbff1adaa185e43da
Will Scott <willscott@gmail.com>
2015-05-07 20:34:41 -0400

    imgs.

--- static/ux.js
+++ static/ux.js
@@ -9,10 +9,13 @@ window.addEventListener('load', function () {
 
 function setupDemo() {
   var ifere = document.getElementById('demo-button');
+  var activ = document.getElementById('act-button');
   var rline = document.getElementsByClassName('right')[0];
   var bcontent = document.getElementById('browserinter');
   ifere.addEventListener('click', function () {
-    rline.style.display = 'none';
-    bcontent.style.display = 'block';
+    var checked = ifere.checked;
+    rline.style.display = checked ? 'none' : 'block';
+    bcontent.style.display = checked ? 'block' : 'none';
+    bcontent.src = activ.checked ? '/interference.png' : '/notfound.png';
   }, false);
 };
