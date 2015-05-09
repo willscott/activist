c21bb57786297821baf24cd9a06ad9875911b95a
Will Scott <willscott@gmail.com>
2015-05-09 14:12:02 -0400

    second page in decent shape.

--- static/ux.js
+++ static/ux.js
@@ -12,10 +12,22 @@ function setupDemo() {
   var activ = document.getElementById('act-button');
   var rline = document.getElementsByClassName('right')[0];
   var bcontent = document.getElementById('browserinter');
-  ifere.addEventListener('click', function () {
-    var checked = ifere.checked;
-    rline.style.display = checked ? 'none' : 'block';
-    bcontent.style.display = checked ? 'block' : 'none';
-    bcontent.src = activ.checked ? '/interference.png' : '/notfound.png';
-  }, false);
+  var ifering = false;
+  var activing = false;
+  var clickifere = function () {
+    ifering = !ifering;
+    ifere.className = ifering ? 'active' : '';
+    rline.style.display = ifering ? 'none' : 'block';
+    bcontent.style.display = ifering ? 'block' : 'none';
+    bcontent.src = activing ? '/interference.png' : '/notfound.png';
+  };
+
+  activ.addEventListener('click', function () {
+    activing = !activing;
+    activ.className = activing ? 'active' : '';
+    if (activing && ifering) {
+      clickifere();
+    }
+  });
+  ifere.addEventListener('click', clickifere, false);
 };
